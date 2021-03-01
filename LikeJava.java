e5381790f40e6f8b08e6f07ad70d48b5682fa8bf
Jackson-eng <71522825+Jackson-eng@users.noreply.github.com>
2021-03-01 07:18:59 -0800

    Added space between Daves Mouth and "Dave Says:"

--- LikeJava.java
+++ LikeJava.java
@@ -5,7 +5,7 @@ public class LikeJava
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
-        String daveSays = "   -  -\n(________)\nDave Says:\n";
+        String daveSays = "   -  -\n(________)\n\nDave Says:\n";
         
         System.out.println(daveSays + "Do you love Java?\n\nYou Say:");
         String imput = scanner.nextLine();
