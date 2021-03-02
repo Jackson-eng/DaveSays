b4bb319adc64dac2f587078793f9d4095e6a2142
Jackson-eng <71522825+Jackson-eng@users.noreply.github.com>
2021-03-02 11:06:46 -0800

    Removed "ln" from "println" (line 5)

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld
 {
     public static void main(String[] args)
     {
-        System.out.println("   -  -\n(________)\n\nDave Says:\nHello World!");
+        System.out.print("   -  -\n(________)\n\nDave Says:\nHello World!");
     }
 }
